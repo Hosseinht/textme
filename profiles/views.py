ec781ff6611f023e0a72dfcd4c107fbe32ec66c2
Ryan Taheri <sailantht@gmail.com>
2021-04-14 16:47:11 +0430

    34.Total Posts

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 
+from feed.models import Post
+
 
 class ProfileDetailView(DetailView):
     http_method_names = ["get"]
@@ -10,5 +12,11 @@ class ProfileDetailView(DetailView):
     context_object_name = "user"
     slug_field = 'username'
     slug_url_kwarg = "username" #It's from url path
+
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_post'] = Post.objects.filter(author=user).count()
+        return context
 
 
