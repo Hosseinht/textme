323217ac8cebfd16d8666ecd1b635f41e7a4258d
Ryan Taheri <sailantht@gmail.com>
2021-04-14 19:52:06 +0430

    37.Dynamic Follow and Unfollow

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'profiles'
 
 urlpatterns = [
-    path('profile/<str:username>/', views.ProfileDetailView.as_view(), name="detail")
+    path('profile/<str:username>/', views.ProfileDetailView.as_view(), name="detail"),
+    path('profile/<str:username>/follow', views.FollowView.as_view(), name="follow"),
 ]
